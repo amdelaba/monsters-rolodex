901ef4d65e84e51694810142eb097dab06e46cff
Andres De La Barra <adelabarra24@gmail.com>
2020-02-17 20:02:53 -0500

    Updates

---
+++ precache-manifest.43422718c35c0ea4ab4d6ced537dd65a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8a6792ee240abf64df1ec856ac274e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "097ff3ef7dc692891141",
+    "url": "/monsters-rolodex/static/css/main.259a86df.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097ff3ef7dc692891141",
+    "url": "/monsters-rolodex/static/js/main.5a2c4bd1.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
